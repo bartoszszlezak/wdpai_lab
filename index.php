17be51f5337b1f2d957bd58b8d7848d1e399f552
bartoszszlezal <barmikszl@gmail.com>
2020-11-19 12:48:47 +0100

    Added receiving data from the login form

--- index.php
+++ index.php
@@ -7,5 +7,7 @@ $path = parse_url( $path, PHP_URL_PATH);
 
 Router::get('index', 'DefaultController');
 Router::get('projects', 'DefaultController');
+Router::post('login', 'SecurityController');
+
 
 Router::run($path);
